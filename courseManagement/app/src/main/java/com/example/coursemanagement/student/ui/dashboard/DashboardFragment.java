f5a99501ea4eb02f54a1a89e11e7b9e9b30d4473
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-11 14:55:23 +0800

    重命名了一下

--- courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/dashboard/DashboardFragment.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/dashboard/DashboardFragment.java
@@ -23,7 +23,7 @@ public class DashboardFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.student_fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
