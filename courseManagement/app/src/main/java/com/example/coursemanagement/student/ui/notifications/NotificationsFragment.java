68f9f8636f833ec05ecefc3a0bc3b4401d501f7e
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-12 00:47:54 +0800

    完成了退出按钮

--- courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/notifications/NotificationsFragment.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,13 @@
 package com.example.coursemanagement.student.ui.notifications;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.coursemanagement.Login;
 import com.example.coursemanagement.R;
 
 
@@ -21,14 +26,22 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.student_fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.student_text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View root = inflater.inflate(R.layout.student_me, container, false);
+
+        Button button = root.findViewById(R.id.student_me_quit_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+
+                String spFileName = getResources().getString(R.string.shared_preferences_file_name);
+                SharedPreferences spFile = getActivity().getSharedPreferences(spFileName, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = spFile.edit();
+                //设置未登录
+                editor.putBoolean(getResources().getString(R.string.logined), false).apply();
+
+                Intent intent = new Intent(getActivity(), Login.class);
+                //调用 activity
+                startActivity(intent);
             }
         });
         return root;
--- courseManagement/app/src/main/java/com/example/coursemanagement/teacher/ui/notifications/NotificationsFragment.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/teacher/ui/notifications/NotificationsFragment.java
@@ -1,17 +1,18 @@
 package com.example.coursemanagement.teacher.ui.notifications;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.example.coursemanagement.Login;
 import com.example.coursemanagement.R;
 
 
@@ -21,14 +22,25 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View root = inflater.inflate(R.layout.teacher_me, container, false);
+
+        Button button=root.findViewById(R.id.teacher_me_quit_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+
+                String spFileName = getResources().getString(R.string.shared_preferences_file_name);
+                SharedPreferences spFile = getActivity().getSharedPreferences(spFileName, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = spFile.edit();
+                //保存身份
+//                editor.remove(getResources().getString(R.string.identity));
+                //设置未登录
+                editor.putBoolean(getResources().getString(R.string.logined),false).apply();
+
+                Intent intent = new Intent(getActivity(), Login.class);
+                //调用 activity
+                startActivity(intent);
             }
         });
         return root;
