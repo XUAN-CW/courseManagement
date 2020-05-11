20ca705d8d51391d3d73d7e679de1fb37cfdaaef
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-11 16:00:49 +0800

    解决了 teacher 页面的模板

---
+++ courseManagement/app/src/main/java/com/example/coursemanagement/teacher/ui/home/MySQLiteOpenHelper.java
@@ -0,0 +1,157 @@
+package com.example.coursemanagement.teacher.ui.home;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.coursemanagement.Network;
+import com.example.coursemanagement.R;
+
+import java.net.HttpURLConnection;
+
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+    private static final String ASSIGNMENT_TABLE =
+            "CREATE TABLE " +
+                    NewsContract.NewsEntry.ASSIGNMENT_TABLE + " (" +
+                    NewsContract.NewsEntry.ASSIGNMENT_COURSE + " VARCHAR(50), " +
+                    NewsContract.NewsEntry.ASSIGNMENT_TITLE + " VARCHAR(50), " +
+                    NewsContract.NewsEntry.ASSIGNMENT_CONTENT + " VARCHAR(500), " +
+                    NewsContract.NewsEntry.ASSIGNMENT_START_TIME + " VARCHAR(50), " +
+                    NewsContract.NewsEntry.ASSIGNMENT_DEADLINE + " VARCHAR(50),"+
+                    NewsContract.NewsEntry.ASSIGNMENT_ASSIGNMENT_NUMBER + " VARCHAR(50),"+
+                    NewsContract.NewsEntry.ASSIGNMENT_COURSE_NUMBER + " VARCHAR(50)"+
+                    " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + NewsContract.NewsEntry.ASSIGNMENT_TABLE;
+
+    public static final int DATABASE_VERSION = 1;
+//    public static final String DATABASE_NAME = "news--"+new Date()+".db";
+    public static final String DATABASE_NAME = "assignment2.db";
+    private Context mContext;
+
+    public MySQLiteOpenHelper(Context context) {
+        super(context , DATABASE_NAME , null , DATABASE_VERSION);
+        mContext = context;
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        System.out.println(4444444);
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        sqLiteDatabase.execSQL(ASSIGNMENT_TABLE);
+        initDb(sqLiteDatabase);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase ,
+                          int oldVersion , int newVersion) {
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
+    }
+
+
+    private void initDb(final SQLiteDatabase sqLiteDatabase) {
+        System.out.println("initDb---------------");
+        new Thread(new Runnable() {//创建子线程
+            @Override
+            public void run() {
+
+                String spFileName = mContext.getResources().getString(R.string.shared_preferences_file_name);
+                String accountKey = mContext.getResources().getString(R.string.login_account_name);
+                SharedPreferences spFile = mContext.getSharedPreferences(spFileName , Context.MODE_PRIVATE);
+                String account = spFile.getString(accountKey , null);
+                account="101";
+
+                try {
+                    HttpURLConnection homeworkFromDatabase = Network.getTeacherAssignment(account);
+                    String data=homeworkFromDatabase.getHeaderField("data");
+                    System.err.println("收到了\n"+data);
+
+                    String homeworkTitle[]=null;
+                    String homeworkContent[]=null;
+                    String course[]=null;
+                    String startTime[]=null;
+                    String deadline[]=null;
+                    String courseNumber[]=null;
+                    String assignmentNumber[]=null;
+                    String columnDivider="bbbbb";
+                    String itemDivider="aaaaa";
+                    String[] temp=data.split(columnDivider);
+
+                    for(int i=0;i<temp.length;i++){
+                        String[] currColumn=temp[i].split(itemDivider);
+//                        System.err.println(currColumn[0]);
+                        if(currColumn[0].equals("title")){
+                            currColumn[0]="homeworkTitle";
+                            homeworkTitle=currColumn;
+                        }
+                        if(currColumn[0].equals("content")){
+                            currColumn[0]="homeworkContent";
+                            homeworkContent=currColumn;
+                        }
+                        if(currColumn[0].equals("name")){
+                            currColumn[0]="course";
+                            course=currColumn;
+                        }
+                        if(currColumn[0].equals("startTime")){
+                            currColumn[0]="startTime";
+                            startTime=currColumn;
+                        }
+                        if(currColumn[0].equals("deadline")){
+                            currColumn[0]="deadline";
+                            deadline=currColumn;
+                        }
+                        if(currColumn[0].equals("courseNumber")){
+                            currColumn[0]="courseNumber";
+                            courseNumber=currColumn;
+                            System.out.println();
+                        }
+                        if(currColumn[0].equals("assignmentNumber")){
+                            currColumn[0]="assignmentNumber";
+                            assignmentNumber=currColumn;
+                        }
+                    }
+                    int length = 0;
+                    length = Math.min(homeworkTitle.length , homeworkContent.length);
+                    length = Math.min(length , course.length);
+                    length = Math.min(length , startTime.length);
+                    length = Math.min(length , deadline.length);
+
+                    for (int i = 1; i < length; i++) {
+                        ContentValues values = new ContentValues();
+
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_COURSE_NUMBER , courseNumber[i]);
+                        System.out.println(courseNumber[i]);
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_ASSIGNMENT_NUMBER , assignmentNumber[i]);
+                        System.out.println(assignmentNumber[i]);
+
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_TITLE , homeworkTitle[i]);
+                        System.out.println(homeworkTitle[i]);
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_CONTENT , homeworkContent[i]);
+                        System.out.println(homeworkContent[i]);
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_COURSE , course[i]);
+                        System.out.println(course[i]);
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_START_TIME , startTime[i]);
+                        System.out.println(startTime[i]);
+                        values.put(NewsContract.NewsEntry.ASSIGNMENT_DEADLINE , deadline[i]);
+                        System.out.println(deadline[i]);
+
+                        long r = sqLiteDatabase.insert(
+                                NewsContract.NewsEntry.ASSIGNMENT_TABLE,
+                                null ,
+                                values);
+                    }
+                }catch (Exception e){
+
+                }}
+        }).start();//启动子线程
+    }
+}
