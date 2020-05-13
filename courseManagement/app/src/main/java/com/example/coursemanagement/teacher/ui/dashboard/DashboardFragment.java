c89133a3ffde424f9e21880187eedec765cbea71
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-13 21:28:37 +0800

    给布置作业添加了监听

--- courseManagement/app/src/main/java/com/example/coursemanagement/teacher/ui/dashboard/DashboardFragment.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/teacher/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,15 @@
 package com.example.coursemanagement.teacher.ui.dashboard;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,14 +20,28 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.coursemanagement.R;
 
+import java.util.Calendar;
+
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.assign_homework, container, false);
+
+
+        EditText et_title=root.findViewById(R.id.assignment_title);
+        EditText et_content=root.findViewById(R.id.assignment_content);
+
+        Button button=root.findViewById(R.id.assign_homework_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
 
         return root;
     }
