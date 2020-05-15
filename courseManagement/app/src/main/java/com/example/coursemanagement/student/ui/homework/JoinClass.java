73ada968dc59c932af8eb61fe0bca011fde44b02
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-15 22:40:06 +0800

    增加了返回功能

--- courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/homework/JoinClass.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/homework/JoinClass.java
@@ -2,11 +2,14 @@ package com.example.coursemanagement.student.ui.homework;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,15 +17,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.coursemanagement.Login;
 import com.example.coursemanagement.Network;
 import com.example.coursemanagement.R;
+import com.example.coursemanagement.student.ui.Student;
 
 import java.net.HttpURLConnection;
 
 public class JoinClass extends AppCompatActivity {
+    Button join_class_Button=null;
+    ImageView join_class_back=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_class);
-        findViewById(R.id.join_class_Button).setOnClickListener(new View.OnClickListener() {
+        join_class_Button=findViewById(R.id.join_class_Button);
+        join_class_back=findViewById(R.id.join_class_back);
+
+        joinClassButtonAddListener();
+        joinClassBackAddListener();
+    }
+    
+    public void joinClassButtonAddListener(){
+        join_class_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String spFileName = getResources().getString(R.string.shared_preferences_file_name);
@@ -42,7 +56,7 @@ public class JoinClass extends AppCompatActivity {
                             Toast.makeText(JoinClass.this,
                                     join_class.getHeaderField("status"), Toast.LENGTH_SHORT).show();
                             Looper.loop();
-                            }catch (Exception e){
+                        }catch (Exception e){
 
                         }
                     }
@@ -51,6 +65,17 @@ public class JoinClass extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void joinClassBackAddListener(){
+        join_class_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinClass.this, Student.class);
+
+                //调用 activity
+                startActivity(intent);
+            }
+        });
     }
 }
