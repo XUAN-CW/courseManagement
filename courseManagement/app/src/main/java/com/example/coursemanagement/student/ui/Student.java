d0b630a627e196e51842d1734b622576c610a689
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-06-19 16:32:24 +0800

    加载失败，也同样进入页面

--- courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/Student.java
+++ courseManagement/app/src/main/java/com/example/coursemanagement/student/ui/Student.java
@@ -41,19 +41,20 @@ public class Student extends AppCompatActivity{
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        System.out.println("com.example.coursemanagement.student.ui.Teacher");
+        System.out.println("com.example.coursemanagement.student.ui.student");
         //在这个地方
         String spFileName = getResources().getString(R.string.shared_preferences_file_name);
         SharedPreferences spFile = getSharedPreferences(spFileName , MODE_PRIVATE);
         SharedPreferences.Editor editor = spFile.edit();
         editor.putBoolean(getResources().getString(R.string.isLoaded),false).apply();
         //在这个地方初始化学生的数据库
-        SQLiteDatabase db=null;
-        MySQLiteOpenHelper myDbHelper = null;
-        myDbHelper = new MySQLiteOpenHelper(Student.this);
-        db = myDbHelper.getReadableDatabase();
+//        SQLiteDatabase db=null;
+//        MySQLiteOpenHelper myDbHelper = null;
+//        myDbHelper = new MySQLiteOpenHelper(Student.this);
+//        db = myDbHelper.getReadableDatabase();
+        new MySQLiteOpenHelper(Student.this).initDb();
 
         //不加载完不进入
-        while (!spFile.getBoolean(getResources().getString(R.string.isLoaded) , false)){}
+//        while (!spFile.getBoolean(getResources().getString(R.string.isLoaded) , false)){}
     }
 }
